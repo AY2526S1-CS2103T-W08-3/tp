adc97d10994014395249c43e4ba8d7ef74cb1a97
kianshenteoh <kianshen12@gmail.com>
2025-10-09 20:38:02 +0800

    Fix checkstyle

--- src/test/java/seedu/address/logic/parser/AddLessonCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/AddLessonCommandParserTest.java
@@ -1,5 +1,7 @@
 package seedu.address.logic.parser;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static seedu.address.logic.commands.CommandTestUtil.DAY_DESC_MATH;
 import static seedu.address.logic.commands.CommandTestUtil.DAY_DESC_SCIENCE;
@@ -28,7 +30,6 @@ import static seedu.address.logic.parser.CliSyntax.PREFIX_STARTTIME;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_VENUE;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 
 import seedu.address.logic.Messages;
