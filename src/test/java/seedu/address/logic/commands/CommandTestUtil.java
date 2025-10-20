bda049984ac5ecb6a6ce198f60f9a1e71eae13a8
Andre Keh <andrekeh@gmail.com>
2025-10-20 16:21:43 +0800

    Implement EditLessonCommand Tests

--- src/test/java/seedu/address/logic/commands/CommandTestUtil.java
+++ src/test/java/seedu/address/logic/commands/CommandTestUtil.java
@@ -27,6 +27,7 @@ import seedu.address.model.lesson.predicates.LessonIdEqualsPredicate;
 import seedu.address.model.person.Person;
 import seedu.address.model.person.UserId;
 import seedu.address.model.person.predicates.UserIdEqualsPredicate;
+import seedu.address.testutil.EditLessonDescriptorBuilder;
 import seedu.address.testutil.EditPersonDescriptorBuilder;
 
 /**
@@ -93,6 +94,10 @@ public class CommandTestUtil {
     public static final EditCommand.EditPersonDescriptor DESC_AMY;
     public static final EditCommand.EditPersonDescriptor DESC_BOB;
 
+    public static final EditLessonCommand.EditLessonDescriptor DESC_MATH;
+    public static final EditLessonCommand.EditLessonDescriptor DESC_SCIENCE;
+
+
     static {
         DESC_AMY = new EditPersonDescriptorBuilder().withName(VALID_NAME_AMY)
                 .withPhone(VALID_PHONE_AMY).withEmail(VALID_EMAIL_AMY).withNote(VALID_STUDENT_NOTE_AMY)
@@ -100,6 +105,12 @@ public class CommandTestUtil {
         DESC_BOB = new EditPersonDescriptorBuilder().withName(VALID_NAME_BOB)
                 .withPhone(VALID_PHONE_BOB).withEmail(VALID_EMAIL_BOB).withNote(VALID_STUDENT_NOTE_BOB)
                 .withTags(VALID_TAG_HUSBAND, VALID_TAG_FRIEND).build();
+        DESC_MATH = new EditLessonDescriptorBuilder().withDay(VALID_DAY_MATH).withStartTime(VALID_STARTTIME_MATH)
+                .withEndTime(VALID_ENDTIME_MATH).withVenue(VALID_VENUE_MATH).withNote(VALID_LESSON_NOTE_MATH)
+                .build();
+        DESC_SCIENCE = new EditLessonDescriptorBuilder().withDay(VALID_DAY_SCIENCE).withStartTime(VALID_STARTTIME_SCIENCE)
+                .withEndTime(VALID_ENDTIME_SCIENCE).withVenue(VALID_VENUE_SCIENCE).withNote(VALID_LESSON_NOTE_SCIENCE)
+                .build();
     }
 
     /**
---
+++ src/test/java/seedu/address/logic/commands/EditLessonCommandTest.java
@@ -0,0 +1,157 @@
+package seedu.address.logic.commands;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static seedu.address.logic.commands.CommandTestUtil.*;
+import static seedu.address.testutil.TypicalAddressBook.getTypicalAddressBook;
+import static seedu.address.testutil.TypicalIndexes.FIRST_INDEX;
+import static seedu.address.testutil.TypicalIndexes.SECOND_INDEX;
+
+import org.junit.jupiter.api.Test;
+
+import seedu.address.commons.core.index.Index;
+import seedu.address.logic.Messages;
+import seedu.address.logic.commands.EditLessonCommand.EditLessonDescriptor;
+import seedu.address.model.AddressBook;
+import seedu.address.model.Model;
+import seedu.address.model.ModelManager;
+import seedu.address.model.UserPrefs;
+import seedu.address.model.lesson.Lesson;
+import seedu.address.testutil.EditLessonDescriptorBuilder;
+import seedu.address.testutil.LessonBuilder;
+
+/**
+ * Contains integration tests (interaction with the Model) and unit tests for EditCommand.
+ */
+public class EditLessonCommandTest {
+
+    private Model model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
+
+    @Test
+    public void execute_allFieldsSpecifiedUnfilteredList_success() {
+        Lesson first = model.getAddressBook().getLessonList().get(0);
+        Lesson editedLesson = new LessonBuilder().withLessonId(first.getLessonId().value).build();
+        EditLessonDescriptor descriptor = new EditLessonDescriptorBuilder(editedLesson).build();
+        EditLessonCommand editLessonCommand = new EditLessonCommand(FIRST_INDEX, descriptor);
+
+        String expectedMessage = String.format(EditLessonCommand.MESSAGE_EDIT_LESSON_SUCCESS, Messages.format(editedLesson));
+
+        Model expectedModel = new ModelManager(new AddressBook(model.getAddressBook()), new UserPrefs());
+
+        expectedModel.setLesson(model.getFilteredLessonList().get(0), editedLesson);
+
+        assertCommandSuccess(editLessonCommand, model, expectedMessage, expectedModel);
+    }
+
+    @Test
+    public void execute_someFieldsSpecifiedUnfilteredList_success() {
+        Index indexLastLesson = Index.fromOneBased(model.getFilteredLessonList().size());
+        Lesson lastLesson = model.getFilteredLessonList().get(indexLastLesson.getZeroBased());
+
+        LessonBuilder lessonInList = new LessonBuilder(lastLesson);
+        Lesson editedLesson = lessonInList.withDay(VALID_DAY_MATH).withStartTime(VALID_STARTTIME_MATH).build();
+
+        EditLessonDescriptor descriptor = new EditLessonDescriptorBuilder()
+                .withDay(VALID_DAY_MATH).withStartTime(VALID_STARTTIME_MATH).build();
+        EditLessonCommand editLessonCommand = new EditLessonCommand(indexLastLesson, descriptor);
+
+        String expectedMessage = String.format(EditLessonCommand.MESSAGE_EDIT_LESSON_SUCCESS, Messages.format(editedLesson));
+
+        Model expectedModel = new ModelManager(new AddressBook(model.getAddressBook()), new UserPrefs());
+        expectedModel.setLesson(lastLesson, editedLesson);
+
+        assertCommandSuccess(editLessonCommand, model, expectedMessage, expectedModel);
+    }
+
+    @Test
+    public void execute_noFieldSpecifiedUnfilteredList_success() {
+        EditLessonCommand editLessonCommand = new EditLessonCommand(FIRST_INDEX, new EditLessonDescriptor());
+        Lesson editedLesson = model.getFilteredLessonList().get(FIRST_INDEX.getZeroBased());
+
+        String expectedMessage = String.format(EditLessonCommand.MESSAGE_EDIT_LESSON_SUCCESS, Messages.format(editedLesson));
+
+        Model expectedModel = new ModelManager(new AddressBook(model.getAddressBook()), new UserPrefs());
+
+        assertCommandSuccess(editLessonCommand, model, expectedMessage, expectedModel);
+    }
+
+    @Test
+    public void execute_filteredList_success() {
+        showLessonAtIndex(model, FIRST_INDEX);
+
+        Lesson lessonInFilteredList = model.getFilteredLessonList().get(FIRST_INDEX.getZeroBased());
+        Lesson editedLesson = new LessonBuilder(lessonInFilteredList).withDay(VALID_DAY_MATH).build();
+        EditLessonCommand editLessonCommand = new EditLessonCommand(FIRST_INDEX,
+                new EditLessonDescriptorBuilder().withDay(VALID_DAY_MATH).build());
+
+        String expectedMessage = String.format(EditLessonCommand.MESSAGE_EDIT_LESSON_SUCCESS, Messages.format(editedLesson));
+
+        Model expectedModel = new ModelManager(new AddressBook(model.getAddressBook()), new UserPrefs());
+        expectedModel.setLesson(model.getFilteredLessonList().get(0), editedLesson);
+
+        assertCommandSuccess(editLessonCommand, model, expectedMessage, expectedModel);
+    }
+
+    @Test
+    public void execute_invalidLessonIndexUnfilteredList_failure() {
+        Index outOfBoundIndex = Index.fromOneBased(model.getFilteredLessonList().size() + 1);
+        EditLessonDescriptor descriptor = new EditLessonDescriptorBuilder().withDay(VALID_DAY_MATH).build();
+        EditLessonCommand editLessonCommand = new EditLessonCommand(outOfBoundIndex, descriptor);
+
+        assertCommandFailure(editLessonCommand, model, Messages.MESSAGE_INVALID_LESSON_DISPLAYED_INDEX);
+    }
+
+    /**
+     * Edit filtered list where index is larger than size of filtered list,
+     * but smaller than size of address book
+     */
+    @Test
+    public void execute_invalidLessonIndexFilteredList_failure() {
+        showLessonAtIndex(model, FIRST_INDEX);
+        Index outOfBoundIndex = SECOND_INDEX;
+        // ensures that outOfBoundIndex is still in bounds of address book list
+        assertTrue(outOfBoundIndex.getZeroBased() < model.getAddressBook().getLessonList().size());
+
+        EditLessonCommand editLessonCommand = new EditLessonCommand(outOfBoundIndex,
+                new EditLessonDescriptorBuilder().withDay(VALID_DAY_MATH).build());
+
+        assertCommandFailure(editLessonCommand, model, Messages.MESSAGE_INVALID_LESSON_DISPLAYED_INDEX);
+    }
+
+    @Test
+    public void equals() {
+        final EditLessonCommand standardCommand = new EditLessonCommand(FIRST_INDEX, DESC_MATH);
+
+        // same values -> returns true
+        EditLessonDescriptor copyDescriptor = new EditLessonDescriptor(DESC_MATH);
+        EditLessonCommand commandWithSameValues = new EditLessonCommand(FIRST_INDEX, copyDescriptor);
+        assertTrue(standardCommand.equals(commandWithSameValues));
+
+        // same object -> returns true
+        assertTrue(standardCommand.equals(standardCommand));
+
+        // null -> returns false
+        assertFalse(standardCommand.equals(null));
+
+        // different types -> returns false
+        assertFalse(standardCommand.equals(new ClearCommand()));
+
+        // different index -> returns false
+        assertFalse(standardCommand.equals(new EditLessonCommand(SECOND_INDEX, DESC_SCIENCE)));
+
+        // different descriptor -> returns false
+        assertFalse(standardCommand.equals(new EditLessonCommand(FIRST_INDEX, DESC_SCIENCE)));
+    }
+
+    @Test
+    public void toStringMethod() {
+        Index index = Index.fromOneBased(1);
+        EditLessonDescriptor editLessonDescriptor = new EditLessonDescriptor();
+        EditLessonCommand editLessonCommand = new EditLessonCommand(index, editLessonDescriptor);
+        String expected = EditLessonCommand.class.getCanonicalName() + "{index=" + index + ", editLessonDescriptor="
+                + editLessonDescriptor + "}";
+        assertEquals(expected, editLessonCommand.toString());
+    }
+
+}
