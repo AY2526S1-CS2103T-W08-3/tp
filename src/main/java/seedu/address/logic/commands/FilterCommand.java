ba06e8b77baf021e27cf89f344a7372e422f5a9c
gunawanjoseph <josephgroups100@gmail.com>
2025-10-30 00:49:16 +0800

    Fix message ussage for filter command

--- src/main/java/seedu/address/logic/commands/FilterCommand.java
+++ src/main/java/seedu/address/logic/commands/FilterCommand.java
@@ -9,8 +9,8 @@ public abstract class FilterCommand extends Command {
 
     public static final String MESSAGE_USAGE = COMMAND_WORD
             + ": filters a list of students by lesson, or a list of lessons by student.\n"
-            + "filter s/NAME INDEX filters the list of lessons by the specified student, while "
-            + "filter l/DAY INDEX filters the list of students by the specified lesson.\n"
+            + "filter n/NAME INDEX filters the list of lessons by the specified student, while "
+            + "filter d/DAY INDEX filters the list of students by the specified lesson.\n"
             + "Omitting the INDEX parameter for either displays the referenced list of students or lessons.\n"
-            + "Example: " + COMMAND_WORD + " s/John 2 or " + COMMAND_WORD + " l/Mon 2.";
+            + "Example: " + COMMAND_WORD + " n/John 2 or " + COMMAND_WORD + " d/Mon 2.";
 }
