131898b96407116cab4073a6a2d4d34798f69218
Andre Keh <andrekeh@gmail.com>
2025-11-02 14:42:15 +0800

    Fix grammar error in listed message

--- src/main/java/seedu/address/logic/Messages.java
+++ src/main/java/seedu/address/logic/Messages.java
@@ -16,8 +16,8 @@ public class Messages {
     public static final String MESSAGE_UNKNOWN_COMMAND = "Unknown command";
     public static final String MESSAGE_INVALID_COMMAND_FORMAT = "Invalid command format! \n%1$s";
     public static final String MESSAGE_INVALID_PERSON_DISPLAYED_INDEX = "The person index provided is invalid";
-    public static final String MESSAGE_PERSONS_LISTED_OVERVIEW = "%1$d persons listed!";
-    public static final String MESSAGE_LESSONS_LISTED_OVERVIEW = "%1$d lessons listed!";
+    public static final String MESSAGE_PERSONS_LISTED_OVERVIEW = "%1$d listed! (Persons)";
+    public static final String MESSAGE_LESSONS_LISTED_OVERVIEW = "%1$d listed! (Lessons)";
     public static final String MESSAGE_DUPLICATE_FIELDS =
                 "Multiple values specified for the following single-valued field(s): ";
     public static final String MESSAGE_NO_USERS_FOUND = "There are no persons matching the name: \"%s\".";
